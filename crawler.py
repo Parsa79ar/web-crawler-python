88015b0231d16935da7a4dfaa0ae082f42c06558
Parsa79ar <parsa.ardebili@gmail.com>
2023-01-28 06:52:07 -0800

    Use selenium instead of requests & soap - hackerone listen functionality

--- crawler.py
+++ crawler.py
@@ -1,11 +1,28 @@
-from bs4 import BeautifulSoup
-import threading
-import requests
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+import threading, time
+
+
+# Threads  
+thread_list = []
 
 
 # Return page with url
 def get_page(url: str) -> object:
-    page = requests.get(url)
-    return page
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    browser = webdriver.Chrome(options=chrome_options)
+    browser.get(url)
+    return browser
 
-print(get_page("https://hackerone.com/directory/programs?order_direction=DESC&order_field=launched_at"))
+
+# Listen for New Content in Hackerone.com (!! Only new programs section !!)
+def hackerone_listen_content():
+    url = "https://hackerone.com/directory/programs?order_direction=DESC&order_field=launched_at"
+    page_content = get_page(url)
+    time.sleep(10)
+    print(page_content)
+
+hackerone_listen_content();
+
